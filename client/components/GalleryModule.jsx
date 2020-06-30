f9d50a1c1b0deeb4d2be000f32bd3f89f2961d7f
Quinn Royston <quinn.royston@gmail.com>
2020-06-29 20:06:50 -0700

    Added components for each of team members module

---
+++ client/components/GalleryModule.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GalleryModule = (props) => (
+  <div id="gallery">
+    <h1>Gallery</h1>
+  </div>
+)
+
+export default GalleryModule;
---
+++ client/components/NearbyListingsModule.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NearbyListingsModule = (props) => (
+  <div id="listingsNear">
+    <h1>Nearby Listings</h1>
+  </div>
+)
+
+export default NearbyListingsModule;
